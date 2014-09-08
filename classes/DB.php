382069406bb81c70f32e1068ae57551e9907b669
Dmitry <thetekst@gmail.com>
2014-09-08 14:38:11 +0400

    Update DB.php
    fix PDOExcep_t_ion

--- classes/DB.php
+++ classes/DB.php
@@ -21,7 +21,7 @@ class DB
 				Config::get('mysql/password')
 			);
 			$this->_pdo->exec('SET NAMES utf8');
-		} catch (PDOExceprion $e) {
+		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
 	}
